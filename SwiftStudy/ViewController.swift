efa495357ce39ed8bea13735613ac2d82e4d334c
sungjin-wie <tmdwls1203@gmail.com>
2022-02-24 19:58:18 +0900

    why click?

--- SwiftStudy/ViewController.swift
+++ SwiftStudy/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var onClickCount: Int = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func onClickButton(_ sender: UIButton) {
+        onClickCount += 1
+        showAlert(message: "외 \(onClickCount)번이나 눌름?")
+    }
+    
+    func showAlert(message: String) {
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
+            }
+            alert.addAction(okAction)
+            present(alert, animated: true, completion: nil)
+        }
 }
 
