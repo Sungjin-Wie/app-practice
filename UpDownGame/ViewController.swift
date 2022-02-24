f86aa6841ba3082356db91e6559f727efa2aedc1
sungjin-wie <tmdwls1203@gmail.com>
2022-02-24 18:44:31 +0900

    chapter 6

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -16,19 +16,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var currentValueLabel: UILabel!
     @IBOutlet weak var minimumValueLabel: UILabel!
     @IBOutlet weak var maximumValueLabel: UILabel!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         slider.setThumbImage(#imageLiteral(resourceName: "slider_thumb"), for: .normal)
         reset()
     }
-
+    
     @IBAction func sliderValueChanged(_ sender: UISlider) {
         currentValueLabel.text = "\(Int(round(slider.value)))"
     }
     
-    @IBAction func touchUpHitButton (_ sender: UIButton) {
+    @IBAction func touchUpHitButton(_ sender: UIButton) {
         let hitValue: Int = Int(round(slider.value))
         slider.value = Float(hitValue)
         tryCount += 1
@@ -48,12 +48,12 @@ class ViewController: UIViewController {
         
     }
     
-    @IBAction func touchUpResetButton (_ sender: UIButton) {
+    @IBAction func touchUpResetButton(_ sender: UIButton) {
         slider.value = (slider.maximumValue + slider.minimumValue) / 2
         reset()
     }
     
-    func reset () {
+    func reset() {
         randomValue = Int.random(in: 0...30)
         print("randomValue reset to \(randomValue)")
         tryCount = 0
@@ -67,7 +67,7 @@ class ViewController: UIViewController {
         
     }
     
-    func showAlert (message: String) {
+    func showAlert(message: String) {
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
             self.reset()
@@ -75,6 +75,6 @@ class ViewController: UIViewController {
         alert.addAction(okAction)
         present(alert, animated: true, completion: nil)
     }
-
+    
 }
 
