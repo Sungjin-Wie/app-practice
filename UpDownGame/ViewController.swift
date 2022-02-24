e9fdb30b01221934feb1b41a188e072544a3b5e0
sungjin-wie <tmdwls1203@gmail.com>
2022-02-24 14:40:12 +0900

    chaper 2

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -8,12 +8,25 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var slider: UISlider!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func sliderValueChanged(_ sender: UISlider) {
+        print(sender.value)
+    }
+    
+    @IBAction func touchUpHitButton (_ sender: UIButton) {
+        print(slider.value)
+    }
+    
+    @IBAction func touchUpResetButton (_ sender: UIButton) {
+        slider.value = (slider.maximumValue + slider.minimumValue) / 2
+    }
 
 }
 
