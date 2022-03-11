f2825e1bb24c8aaf7865c01be99bc8dcc70a4561
sungjin-wie <tmdwls1203@gmail.com>
2022-03-11 17:58:24 +0900

    commit

--- CodeWithStoryBoard/FirstPageViewController.swift
+++ CodeWithStoryBoard/FirstPageViewController.swift
@@ -9,18 +9,46 @@ import Foundation
 import UIKit
 import WebKit
 
-class FirstPageViewController: UIViewController {
-    @IBOutlet weak var webView: WKWebView!
-    override func viewDidLoad() {
+class FirstPageViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    let refreshControl = UIRefreshControl()
+    @IBOutlet weak var wkWebView: WKWebView!
+    public override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        configure()
+        configureWebView()
     }
     
-    func configure() {
-        if let url = URL(string: "https://naver.com") {
-            let request = URLRequest(url: url)
-            webView.load(request)
-        }
+    func configureWebView() {
+        wkWebView.uiDelegate = self
+        wkWebView.navigationDelegate = self
+        view.addSubview(wkWebView!)
+        let url = URL(string: "https://ontheflea.com")!
+        wkWebView.load(URLRequest(url: url))
+        refreshControl.addTarget(self, action: #selector(reloadWebView(_:)), for: .valueChanged)
+        wkWebView.scrollView.addSubview(refreshControl)
     }
+    @objc func reloadWebView(_ sender: UIRefreshControl) {
+           wkWebView.reload()
+           sender.endRefreshing()
+       }
+
+    func webView(_ webView: WKWebView,
+                 runJavaScriptAlertPanelWithMessage message: String,
+                 initiatedByFrame frame: WKFrameInfo,
+                 completionHandler: @escaping () -> Void) {
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        let title = NSLocalizedString("OK", comment: "OK Button")
+        let ok = UIAlertAction(title: title, style: .default) { (action: UIAlertAction) -> Void in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(ok)
+        present(alert, animated: true)
+        completionHandler()
+    }
+
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        wkWebView.evaluateJavaScript("""
+//alert("안녕하세요.")
+""", completionHandler: nil)
+    }
+    
 }
--- CodeWithStoryBoard/SecondPageViewController.swift
+++ CodeWithStoryBoard/SecondPageViewController.swift
@@ -7,10 +7,39 @@
 
 import Foundation
 import UIKit
+import Alamofire
+
+struct Todo: Codable {
+    let completed: Int
+    let id: Int
+    let title: String
+    let userId: Int
+}
+
+struct APIResponse: Codable {
+    let todos: [Todo]
+}
 
 class SecondPageViewController: UIViewController {
+    let tableView = UITableView()
+    var dataSource: [Todo] = []
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        AF.request("https://jsonplaceholder.typicode.com/todos").responseJSON() { (response) in
+            switch response.result {
+            case .success(let res): do {
+                let jsonData = try JSONSerialization.data(withJSONObject: res, options: .prettyPrinted)
+                print(jsonData)
+                let json = try JSONDecoder().decode(APIResponse.self, from: jsonData)
+                print(json)
+            } catch(let err) {
+                print(err.localizedDescription)
+            }
+            case .failure(let error):
+                print("Error: \(error)")
+                return
+            }
+        }
     }
 }
