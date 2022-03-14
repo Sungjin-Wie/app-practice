220f81ad8614853c3e0f92ec162b1f3000be47bf
sungjin-wie <tmdwls1203@gmail.com>
2022-03-14 17:25:31 +0900

    use JSONDecoder to parse json

--- CodeWithStoryBoard/SecondPageViewController.swift
+++ CodeWithStoryBoard/SecondPageViewController.swift
@@ -10,22 +10,19 @@ import UIKit
 import Alamofire
 
 struct Todo: Codable {
-    let completed: Int
+    let completed: Bool
     let id: Int
     let title: String
     let userId: Int
 }
 
-struct APIResponse: Codable {
-    let todos: [Todo]
-}
 
 class SecondPageViewController: UIViewController {
     let tableView = UITableView()
     let scrollView = UIScrollView()
     let mainStackView = UIStackView()
-
-    var userLists: [[String: Any]] = []
+    
+    var userLists: [Todo] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,41 +31,8 @@ class SecondPageViewController: UIViewController {
         getData()
     }
     
-    func createButtons(text: String, color: UIColor = .blue) -> UIButton{
-        let button = UIButton(type: .system)
-        button.setTitle(text, for: .normal)
-        button.setTitleColor(color, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        button.contentHorizontalAlignment = .center
-        return button
-    }
     
-    func setupButtons(list todoList: [[String: Any]]){
-        print(todoList)
-//        let buttonTitles = todos.map{ todo in
-//            print(todo)
-//            return todo["id"]
-//        }
-        let buttonTitles = todoList.map{ todo in
-            todo["title"]
-        }
-
-        let buttonStack = UIStackView()
-        buttonStack.translatesAutoresizingMaskIntoConstraints = false
-        buttonStack.distribution = .equalSpacing
-        buttonStack.axis = .vertical
-        buttonStack.alignment = .fill
-        buttonTitles.forEach { title in
-            buttonStack.addArrangedSubview(createButtons(text: title as! String))
-        }
-        mainStackView.addArrangedSubview(buttonStack)
-        NSLayoutConstraint.activate([
-            buttonStack.widthAnchor.constraint(equalTo: mainStackView.widthAnchor),
-            buttonStack.heightAnchor.constraint(equalTo: mainStackView.heightAnchor)
-        ])
-    }
-    func setupMainStackView(list todoList: [[String: Any]]){
+    func setupMainStackView(list todoList: [Todo]){
         mainStackView.axis = .vertical
         mainStackView.distribution = .equalSpacing
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
@@ -85,10 +49,41 @@ class SecondPageViewController: UIViewController {
         ])
         setupButtons(list: todoList)
     }
-
+    
+    func createButtons(todo: Todo, color: UIColor = .blue) -> UIButton{
+        let button = UIButton(configuration: .filled())
+        button.setTitle(todo.title, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.contentHorizontalAlignment = .center
+        return button
+    }
+    
+    func setupButtons(list todoList: [Todo]){
+        let stackView = UIStackView()
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.distribution = .equalSpacing
+        stackView.spacing = 30
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        //        addHeightPaddingToArrangedSubView(to: stackView, height: 30)
+        todoList.forEach { todo in
+            let button = createButtons(todo: todo)
+            stackView.addArrangedSubview(button)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -100).isActive = true
+            //            addHeightPaddingToArrangedSubView(to: stackView, height: 30)
+        }
+        mainStackView.addArrangedSubview(stackView)
+        NSLayoutConstraint.activate([
+            stackView.widthAnchor.constraint(equalTo: mainStackView.widthAnchor),
+            stackView.heightAnchor.constraint(equalTo: mainStackView.heightAnchor)
+        ])
+    }
+    
     func setupScrollView(){
         scrollView.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(scrollView) // 1
+        view.addSubview(scrollView)
         let frameLayoutGuide = scrollView.frameLayoutGuide
         NSLayoutConstraint.activate([
             frameLayoutGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor),
@@ -97,7 +92,7 @@ class SecondPageViewController: UIViewController {
             frameLayoutGuide.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
     }
-
+    
     
     func getData() {
         let url = "https://jsonplaceholder.typicode.com/todos"
@@ -107,16 +102,18 @@ class SecondPageViewController: UIViewController {
                    encoding: URLEncoding.default,
                    headers: ["Content-Type":"application/json", "Accept":"application/json"])
             .validate(statusCode: 200..<300)
-            .responseJSON { (response) in
+            .responseData { (response) in
                 switch response.result {
-                case .success(let value):
-                    if let resArray = value as? [[String: Any]]{
-                        self.userLists = resArray
-                        self.setupMainStackView(list: resArray)
-                        }
-                default:
-                    break
-                }
+                case .success(let data):
+                    do {
+                        self.userLists = try JSONDecoder().decode([Todo].self, from: data)
+                        self.setupMainStackView(list: self.userLists)
+                    } catch {
+                        print(error)
+                    }
+            default:
+                break
             }
     }
+}
 }
