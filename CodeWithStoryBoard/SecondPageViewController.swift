2ac21a52868efe61acb5abca196ea3ac5581c89f
sungjin-wie <tmdwls1203@gmail.com>
2022-03-14 20:57:46 +0900

    add refresh button

--- CodeWithStoryBoard/SecondPageViewController.swift
+++ CodeWithStoryBoard/SecondPageViewController.swift
@@ -15,27 +15,51 @@ struct Todo: Codable {
     let title: String
     let userId: Int
 }
+let url = "https://jsonplaceholder.typicode.com/todos"
+let fetchTodos = AF.request(url,
+                            method: .get,
+                            parameters: nil,
+                            encoding: URLEncoding.default,
+                            headers: ["Content-Type":"application/json", "Accept":"application/json"])
+    .validate(statusCode: 200..<300)
+    .serializingDecodable([Todo].self)
 
 
 class SecondPageViewController: UIViewController {
-    let tableView = UITableView()
     let scrollView = UIScrollView()
     let mainStackView = UIStackView()
+    let refreshButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.setTitle("새로고침", for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        button.contentHorizontalAlignment = .center
+        button.addTarget(self, action: #selector(onTouchRefreshButton), for: .touchUpInside)
+        return button
+    }()
     var todoLists: [Todo] = []
+    @IBOutlet weak var secondTabBarbutton: UITabBarItem!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        LoadingService.showLoading()
-        setupScrollView()
-        getData()
+        Task {
+            Loading.show()
+            setupScrollView()
+            try? await getData()
+            Loading.hide()
+            setupMainStackView()
+            putDataIntoStackView()
+        }
     }
     
-    
-    func setupMainStackView(){
+    func setupMainStackView() {
         mainStackView.axis = .vertical
         mainStackView.distribution = .equalSpacing
+        mainStackView.spacing = 30
+        mainStackView.axis = .vertical
         mainStackView.translatesAutoresizingMaskIntoConstraints = false
+        mainStackView.alignment = .center
         scrollView.addSubview(mainStackView)
         let contentLayoutGuide = scrollView.contentLayoutGuide
         NSLayoutConstraint.activate([
@@ -45,24 +69,9 @@ class SecondPageViewController: UIViewController {
             mainStackView.trailingAnchor.constraint(equalTo: contentLayoutGuide.trailingAnchor),
             mainStackView.bottomAnchor.constraint(equalTo: contentLayoutGuide.bottomAnchor)
         ])
-        let stackView = UIStackView()
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.distribution = .equalSpacing
-        stackView.spacing = 30
-        stackView.axis = .vertical
-        stackView.alignment = .center
-        self.todoLists.forEach { todo in
-            let button = createButtons(todo: todo)
-            stackView.addArrangedSubview(button)
-            button.translatesAutoresizingMaskIntoConstraints = false
-            button.widthAnchor.constraint(equalTo: stackView.widthAnchor, constant: -100).isActive = true
-        }
-        mainStackView.addArrangedSubview(stackView)
-        NSLayoutConstraint.activate([
-            stackView.widthAnchor.constraint(equalTo: mainStackView.widthAnchor),
-            stackView.heightAnchor.constraint(equalTo: mainStackView.heightAnchor)
-        ])
     }
+    
+    
     
     func createButtons(todo: Todo, color: UIColor = .blue) -> UIButton{
         let button = UIButton(configuration: .filled())
@@ -77,37 +86,62 @@ class SecondPageViewController: UIViewController {
     func setupScrollView(){
         scrollView.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(scrollView)
+        view.addSubview(refreshButton)
+        refreshButton.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.alwaysBounceVertical = true
+        scrollView.bounces  = true
+//        scrollView.refreshControl = UIRefreshControl()
+//        // add target to UIRefreshControl
+//        scrollView.refreshControl?.addTarget(self, action: #selector(callPullToRefresh), for: .valueChanged)
         let frameLayoutGuide = scrollView.frameLayoutGuide
         NSLayoutConstraint.activate([
             frameLayoutGuide.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             frameLayoutGuide.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             frameLayoutGuide.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            frameLayoutGuide.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            frameLayoutGuide.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            refreshButton.topAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            refreshButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            refreshButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
         ])
     }
     
-    
-    func getData() {
-        let url = "https://jsonplaceholder.typicode.com/todos"
-        AF.request(url,
-                   method: .get,
-                   parameters: nil,
-                   encoding: URLEncoding.default,
-                   headers: ["Content-Type":"application/json", "Accept":"application/json"])
-            .validate(statusCode: 200..<300)
-            .responseData { (response) in
-                switch response.result {
-                case .success(let data):
-                    do {
-                        self.todoLists = try JSONDecoder().decode([Todo].self, from: data)
-                        self.setupMainStackView()
-                        LoadingService.hideLoading()
-                    } catch {
-                        print(error)
-                    }
-            default:
-                break
+    @objc func onTouchRefreshButton() {
+        Task {
+            Loading.show()
+            mainStackView.arrangedSubviews.forEach { subView in
+                mainStackView.removeArrangedSubview(subView)
+                subView.removeFromSuperview()
             }
+            mainStackView.removeFromSuperview()
+            Loading.show()
+            try? await getData()
+            self.todoLists = self.todoLists.shuffled()
+            setupMainStackView()
+            putDataIntoStackView()
+            Loading.hide()
+        }
     }
-}
+    
+    func putDataIntoStackView() {
+        print("putDataIntoStackView() started")
+        self.todoLists.forEach { todo in
+            let button = createButtons(todo: todo)
+            mainStackView.addArrangedSubview(button)
+            button.translatesAutoresizingMaskIntoConstraints = false
+            button.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor, constant: -100).isActive = true
+        }
+        print("putDataIntoStackView() completed")
+        Loading.hide()
+    }
+    
+    func getData() async throws -> Void {
+        do {
+            print("getData() started")
+            let result = try await fetchTodos.value
+            self.todoLists = result
+            print("getData() completed")
+        } catch (let error) {
+            print(error)
+        }
+    }
 }
--- CodeWithStoryBoard/loading.swift
+++ CodeWithStoryBoard/loading.swift
@@ -8,30 +8,24 @@
 import Foundation
 import UIKit
 
-class LoadingService {
-    static func showLoading() {
+class Loading {
+    static func show() {
         DispatchQueue.main.async {
-            // 아래 윈도우는 최상단 윈도우
             guard let window = UIApplication.shared.windows.last else { return }
-
             let loadingIndicatorView: UIActivityIndicatorView
-            // 최상단에 이미 IndicatorView가 있는 경우 그대로 사용.
             if let existedView = window.subviews.first(
                 where: { $0 is UIActivityIndicatorView } ) as? UIActivityIndicatorView {
                 loadingIndicatorView = existedView
-            } else { // 새로 만들기.
+            } else {
                 loadingIndicatorView = UIActivityIndicatorView(style: .large)
-                // 아래는 다른 UI를 클릭하는 것 방지.
                 loadingIndicatorView.frame = window.frame
-                loadingIndicatorView.color = .brown
-
                 window.addSubview(loadingIndicatorView)
             }
             loadingIndicatorView.startAnimating()
         }
     }
 
-    static func hideLoading() {
+    static func hide() {
         DispatchQueue.main.async {
             guard let window = UIApplication.shared.windows.last else { return }
             window.subviews.filter({ $0 is UIActivityIndicatorView })
